a99161f69a9a31234412abecfbd79438d0c43642
vipulgarg192 <vipulgarg192@gmail.com>
2020-06-16 19:43:23 -0400

    panda movement added with coin and spikes node attached

--- Pandaton/GameScene.swift
+++ Pandaton/GameScene.swift
@@ -11,36 +11,118 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
     private var lastUpdateTime : TimeInterval = 0
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
-    override func sceneDidLoad() {
+    let hero = SKSpriteNode(imageNamed: "hero")
+    let background = SKSpriteNode(imageNamed: "bg")
+    let spikes = SKSpriteNode(imageNamed: "spikes_new")
+    let coin = SKSpriteNode(imageNamed: "coin")
+    
+    let zombieMovePointsPerSec: CGFloat = 480.0
+    var velocity = CGPoint.zero
+    let playableRect: CGRect
+    
+    var dt: TimeInterval = 0
+    let cameraNode = SKCameraNode()
+    
+    var lives = 3
+    var coinCollected = false
+    var gameOver = false
+    
+    let livesLabel = SKLabelNode(fontNamed: "Superclarendon-Black")
+    let levelLabel = SKLabelNode(fontNamed: "Superclarendon-Black")
+    
+    
+    override init(size: CGSize) {
+        let maxAspectRatio:CGFloat = 16.0/9.0
+        let playableHeight = size.width / maxAspectRatio
+        let playableMargin = (size.height-playableHeight)/2.0
+        playableRect = CGRect(x: 0, y: playableMargin,
+                          width: size.width,
+                          height: playableHeight)
+       
+        super.init(size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func debugDrawPlayableArea() {
+           let shape = SKShapeNode()
+           let path = CGMutablePath()
+           path.addRect(playableRect)
+           shape.path = path
+           shape.strokeColor = SKColor.red
+           shape.lineWidth = 4.0
+           addChild(shape)
+          }
+       
+    
+    override func didMove(to view: SKView) {
+       self.debugDrawPlayableArea()
+       let backgroundTexture = SKTexture(imageNamed: "bg")
 
-        self.lastUpdateTime = 0
+       let background = SKSpriteNode(texture: backgroundTexture)
+       background.zPosition = -1
+        background.size = CGSize(width: self.frame.size.width  , height: self.frame.size.height * 0.8 )
+       background.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+       background.position = CGPoint(x:  size.width/2, y: size.height/2)
+       addChild(background)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        spikes.position = CGPoint(x:  size.width/2, y: 340)
+        spikes.setScale(0.8)
+        addChild(spikes)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        coin.position = CGPoint(x:  size.width/2, y: 600)
+        coin.setScale(0.5)
+        addChild(coin)
+         
+        
+      
+        spawnHero()
+        
+        addChild(cameraNode)
+           camera = cameraNode
+           cameraNode.position = CGPoint(x: size.width/2, y: size.height/2)
+        
+        livesLabel.text = "Lives: X"
+         livesLabel.fontSize = 100
+        livesLabel.zPosition = 150
+         livesLabel.position = CGPoint(
+                      x: playableRect.size.width - CGFloat(320),
+                       y: playableRect.size.height - CGFloat(20))
+                self.addChild(livesLabel)
+        
+        levelLabel.text = "Coins: X"
+                levelLabel.fontSize = 100
+               levelLabel.zPosition = 150
+                levelLabel.position = CGPoint(
+                             x:  CGFloat(320),
+                              y: playableRect.size.height - CGFloat(20))
+                       self.addChild(levelLabel)
+        
+    }
+    
+    func spawnHero() {
+       
+        hero.position = CGPoint(
+          x: hero.size.width/4,
+          y: 370)
+        hero.zPosition = 50
+        hero.name = "hero"
+        addChild(hero)
+        
+        let actionMove =
+          SKAction.moveBy(x: size.width, y: 0, duration: 2.0)
+        let actionrught =
+        SKAction.moveBy(x: -(size.width ), y: 0, duration: 2.0)
+        hero.run(SKAction.repeatForever(SKAction.sequence([actionMove, actionrught])))
+
+
     }
     
     
@@ -68,43 +150,120 @@ class GameScene: SKScene {
         }
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            for touch in touches {
+                       let location = touch.location(in: self)
+                    print(location)
+                let jumpUpAction = SKAction.moveBy(x: 0, y: 300, duration: 0.3)
+                // move down 20
+                let jumpDownAction = SKAction.moveBy(x: 0, y: -300, duration: 0.3)
+                // sequence of move yup then down
+                let jumpSequence = SKAction.sequence([jumpUpAction, jumpDownAction])
+
+                // make player run sequence
+                hero.run(jumpSequence)
+//                moveZombieToward()
+                }
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+//        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
     
     override func update(_ currentTime: TimeInterval) {
+        
+        if lastUpdateTime > 0 {
+             dt = currentTime - lastUpdateTime
+           } else {
+             dt = 0
+           }
+           lastUpdateTime = currentTime
         // Called before each frame is rendered
         
         // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
         
         // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
         self.lastUpdateTime = currentTime
+        
+//        move(sprite: hero, velocity: velocity)
+        boundsCheckZombie()
+        
+    }
+    
+    func move(sprite: SKSpriteNode, velocity: CGPoint) {
+      let amountToMove = CGPoint(x: velocity.x * CGFloat(dt),
+                                 y: velocity.y * CGFloat(dt))
+        let moveleft =
+            SKAction.move(to: CGPoint(x: frame.width, y: 370), duration: 3)
+//        let moveRight =
+//        SKAction.move(to: CGPoint(x: frame.width, y: 370), duration: 3)
+//      sprite.position += moveleft
+    }
+    
+    func moveZombieToward() {
+        let offset = CGPoint(x: frame.size.width, y: 0) - hero.position
+        let direction = offset.normalized()
+        velocity = direction * zombieMovePointsPerSec
+    }
+    
+    func boundsCheckZombie() {
+      let bottomLeft = CGPoint(x: playableRect.minX, y: playableRect.minY)
+      let topRight = CGPoint(x: playableRect.maxX, y: playableRect.maxY)
+      
+      if hero.position.x <= bottomLeft.x {
+        hero.position.x = bottomLeft.x
+        velocity.x = abs(velocity.x)
+      }
+      if hero.position.x >= topRight.x {
+        hero.position.x = topRight.x
+        velocity.x = -velocity.x
+      }
+      if hero.position.y <= bottomLeft.y {
+        hero.position.y = bottomLeft.y
+        velocity.y = -velocity.y
+      }
+      if hero.position.y >= topRight.y {
+        hero.position.y = topRight.y
+        velocity.y = -velocity.y
+      }
+    }
+    
+    func moveCamera() {
+      let backgroundVelocity =
+        CGPoint(x: 480, y: 0)
+      let amountToMove = backgroundVelocity * CGFloat(dt)
+      cameraNode.position += amountToMove
+      
+      enumerateChildNodes(withName: "background") { node, _ in
+        let background = node as! SKSpriteNode
+        if background.position.x + background.size.width <
+            self.cameraRect.origin.x {
+          background.position = CGPoint(
+            x: background.position.x + background.size.width*2,
+            y: background.position.y)
+        }
+      }
+      
+    }
+    
+    var cameraRect : CGRect {
+      let x = cameraNode.position.x - size.width/2
+          + (size.width - playableRect.width)/2
+      let y = cameraNode.position.y - size.height/2
+          + (size.height - playableRect.height)/2
+      return CGRect(
+        x: x,
+        y: y,
+        width: playableRect.width,
+        height: playableRect.height)
     }
 }
